88b50cab2f90261addb0a73418b9b10dfa9bce15
Monotosh Das <monotosh.das@videonetics.com>
2020-12-26 12:15:57 +0530

    modified:   run_check_cuda.sh

--- run_check_cuda.sh
+++ run_check_cuda.sh
@@ -1,2 +1,2 @@
 docker build . -f run_check_cuda.dockerfile -t check_cuda
-docker run -v `pwd`/session:/session --gpus=all check_cuda bash
+docker run -d -v `pwd`/session:/session --gpus=all check_cuda
