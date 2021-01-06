eac5521c95596be2d1fc56bc464eac2420ab8205
Monotosh Das <monotosh.das@videonetics.com>
2021-01-06 13:58:54 +0000

    influxdb added

--- check_cuda/log_cpu_gpu_usage.py
+++ check_cuda/log_cpu_gpu_usage.py
@@ -39,40 +39,66 @@ class LogCpuGpuUsage(Thread):
             i += 1
 
         LOGGER_CPU_USAGE.info(header)
+        db_name = "Hajmola"
+        measurement_name = "cpu_gpu"
         client = None
         try:
-            client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
+            client = InfluxDBClient('192.168.1.174', 8086, 'admin', 'admin123')
+            list_of_database = client.get_list_database()
+            list_of_database = [values for item in list_of_database for key, values in item.items()]
+
+            if db_name not in list_of_database:
+                LOGGER.info("Create database: " + db_name)
+                client.create_database(db_name)
+                # self.__client.create_retention_policy('sajag_policy',
+                #                                       '4w',
+                #                                       3,
+                #                                       database=self.__db_name)
+            client.close()
+            client = InfluxDBClient('192.168.1.156', 8086, 'admin', 'admin123', database=db_name)
         except Exception as e:
             print(e)
 
         while True:
+            s_influx = (f"cpu_percent={obj.cpu.cpu_percent},"
+                        f"cpu_memory_usage_percent={obj.cpu.cpu_memory_usage_percent}")
             s = f"{obj.cpu.cpu_percent},{obj.cpu.cpu_memory_usage_percent},"
             i = 0
             for gpu in obj.gpus:
-                s += f"#GPU{str(i)},{str(gpu.index)},{str(gpu.uuid)},{str(gpu.name)},{str(gpu.utilization_gpu)},{str(gpu.utilization_enc)},{str(gpu.utilization_dec)},{str(gpu.memory_used)},{str(gpu.memory_total)},"
+                s += (f"#GPU{str(i)},"
+                      f"{str(gpu.index)},"
+                      f"{str(gpu.uuid)},"
+                      f"{str(gpu.name)},"
+                      f"{str(gpu.utilization_gpu)},"
+                      f"{str(gpu.utilization_enc)},"
+                      f"{str(gpu.utilization_dec)},"
+                      f"{str(gpu.memory_used)},"
+                      f"{str(gpu.memory_total)},")
                 i += 1
 
             i = 0
             for process in obj.processes:
-                s += f"#PROCESS{str(i)},{str(process.pid)},{str(process.command)},{str(process.cpu_percent)},{str(process.cpu_memory_usage_mib)},{str(process.gpu_id)},{str(process.gpu_memory_usage_mib)},"
+                s += (f"#PROCESS{str(i)},"
+                      f"{str(process.pid)},"
+                      f"{str(process.command)},"
+                      f"{str(process.cpu_percent)},"
+                      f"{str(process.cpu_memory_usage_mib)},"
+                      f"{str(process.gpu_id)},"
+                      f"{str(process.gpu_memory_usage_mib)},")
                 i += 1
             LOGGER_CPU_USAGE.info(s)
             if client:
-                json_data = [
-                    {
-                        "measurement": "cpu_gpu",
-                        "tags": {
-                            "host": host_name,
-                            "region": "us-west"
-                        },
-                        "time": time.time(),
-                        "fields": obj.to_json()
-                    }
-                ]
-                #print(json_body)
-                #print(s)
-
-                # client.write_points(json_data)
+                data = []
+                t = int(time.time()*1000)
+                data_point = f"{measurement_name},host={host_name} {s_influx} {t}"
+                data.append(data_point)
+                # print(data_point)
+                print(s)
+                try:
+                    client.write_points(data, time_precision="ms", batch_size=1, protocol="line")
+                except Exception as e:
+                    print(e)
+                    client = None
             if self.__is_stop.wait(1.0):
                 break
             else:
