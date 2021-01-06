c3a7b4fb6cda5e7df3de75c61d06c5439501fd53
Monotosh Das <monotosh.das@videonetics.com>
2021-01-06 12:44:29 +0000

    influxdb added

--- check_cuda/log_cpu_gpu_usage.py
+++ check_cuda/log_cpu_gpu_usage.py
@@ -1,12 +1,10 @@
 import logging
+import time
 from threading import Event, Thread
 
-import psutil
+from influxdb import InfluxDBClient
 
-from .class_object_flattener import (get_flatten_keys, get_flatten_keys_list,
-                                     get_flatten_values_list)
 from . import controllers
-from .controllers import get_system_info, get_system_status
 
 LOGGER = logging.getLogger(__name__)
 LOGGER_CPU_USAGE = logging.getLogger("cpu_usage")
@@ -24,7 +22,9 @@ class LogCpuGpuUsage(Thread):
 
     def run(self) -> None:
         obj = controllers.get_system_info()
-        LOGGER_CPU_USAGE.info(f"============== Start ================ {obj.host_name}, {obj.os}")
+        host_name = obj.host_name
+        host_os = obj.os
+        LOGGER_CPU_USAGE.info(f"============== Start ================ {host_name}, {host_os}")
 
         obj = controllers.get_system_status()
         header = "cpu_percent,cpu_memory_usage_percent,"
@@ -38,9 +38,14 @@ class LogCpuGpuUsage(Thread):
             header += f"#PROCESS{str(i)},pid,command,cpu_percent,cpu_memory_usage_mib,gpu_id,gpu_memory_usage_mib,"
             i += 1
 
-
         LOGGER_CPU_USAGE.info(header)
-        while True:            
+        client = None
+        try:
+            client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
+        except Exception as e:
+            print(e)
+
+        while True:
             s = f"{obj.cpu.cpu_percent},{obj.cpu.cpu_memory_usage_percent},"
             i = 0
             for gpu in obj.gpus:
@@ -50,15 +55,31 @@ class LogCpuGpuUsage(Thread):
             i = 0
             for process in obj.processes:
                 s += f"#PROCESS{str(i)},{str(process.pid)},{str(process.command)},{str(process.cpu_percent)},{str(process.cpu_memory_usage_mib)},{str(process.gpu_id)},{str(process.gpu_memory_usage_mib)},"
-                i += 1            
+                i += 1
             LOGGER_CPU_USAGE.info(s)
+            if client:
+                json_data = [
+                    {
+                        "measurement": "cpu_gpu",
+                        "tags": {
+                            "host": host_name,
+                            "region": "us-west"
+                        },
+                        "time": time.time(),
+                        "fields": obj.to_json()
+                    }
+                ]
+                #print(json_body)
+                #print(s)
+
+                # client.write_points(json_data)
             if self.__is_stop.wait(1.0):
                 break
             else:
                 obj = controllers.get_system_status()
                 continue
-            
-
+        if client:
+            client.close()
         LOGGER_CPU_USAGE.info("============== End   ================")
 
     def stop(self):
